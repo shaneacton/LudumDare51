a315d6c81b36fb6c602b2697bc2152c1b8bbc2d7
shaneacton <shaneacton745.sa@gmail.com>
2022-10-01 23:34:53 +0200

    highlighted nearest spawn

--- Assets/Scripts/Map/Tile.cs
+++ Assets/Scripts/Map/Tile.cs
@@ -8,8 +8,20 @@ public class Tile : MonoBehaviour
     public int X = -1;
     public int Y = -1;
 
+    public Renderer renderer;
+
+    private void Awake()
+    {
+        renderer = transform.GetChild(0).gameObject.GetComponent<Renderer>();
+    }
+
     public override string ToString()
     {
         return "Tile " + X + ", " + Y + " at pos: " + transform.position;
+    }
+
+    public void setColour(Color col)
+    {
+        renderer.material.SetColor("_Color", col);
     }
 }
