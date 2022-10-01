383f49cb34f5ce3ce633c26611e4599ed334f847
Sasha Abramowitz <reallysasha@gmail.com>
2022-10-01 10:49:25 +0200

    Ghost movement working

--- Assets/Scripts/Ghost/Ghost.cs
+++ Assets/Scripts/Ghost/Ghost.cs
@@ -2,23 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Ghost : MonoBehaviour
+class Ghost : MonoBehaviour
 {
-    List<MovementData> movements;
+    public List<MovementData> movements = new List<MovementData>();
     private int _i;
+
+    private float _worldTime;
+    private bool _once;
     void Start()
     {
-        transform.position = movements[0].position;
-        transform.rotation = movements[0].rotation;
-        _i = 1;
+        if (movements.Count != 0)
+        {
+            transform.position = movements[0].position;
+            transform.rotation = movements[0].rotation;
+            _i = 1;
+        }
     }
 
     void FixedUpdate()
     {
-        transform.position = movements[_i].position;
-        transform.rotation = movements[_i].rotation;
+        if (movements.Count != 0)
+        {
+            transform.position = movements[_i].position;
+            transform.rotation = movements[_i].rotation;
+            _i++;
+        }
+        
         // TODO shooting
-
-        _i++;
     }
 }
--- Assets/Scripts/Ghost/MovementRecorder.cs
+++ Assets/Scripts/Ghost/MovementRecorder.cs
@@ -1,17 +1,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MovementRecorder : MonoBehaviour
+class MovementRecorder : MonoBehaviour
 {
-    List<MovementData> movements;
-
+    public List<MovementData> movements;
+    private float _worldTime = 0;
+    private bool _once = true;
+    public Ghost ghost;
     private void Start()
     {
         movements = new List<MovementData>();
-        movements.Add(new  MovementData(transform.position, transform.rotation, false));
+        movements.Add(new MovementData(transform.position, transform.rotation, false));
     }
     private void FixedUpdate()
     {
         movements.Add(new MovementData(transform.position, transform.rotation, false));
+
+
+        _worldTime += Time.fixedDeltaTime;
+
+        if (_worldTime > 5 && _once)
+        {
+            ghost.movements = movements;
+            _once = false;
+        }
     }
 }
