b35040ebfc93b9f857630df234536890a6a1fdca
shaneacton <shaneacton745.sa@gmail.com>
2022-10-02 10:39:21 +0200

    generic pickup script

---
+++ Assets/Scripts/Enemy/PickUp.cs
@@ -0,0 +1,32 @@
+﻿
+using UnityEngine;
+
+public abstract class PickUp: MonoBehaviour
+{
+    public float lifespanSeconds=5;
+    private float startTime;
+
+    public abstract void pickUp();
+
+    private void Start()
+    {
+        startTime = Time.time;
+    }
+    
+    private void Update()
+    {
+        if (Time.time - startTime > lifespanSeconds)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.transform.CompareTag("Player"))
+        {
+            pickUp();
+            Destroy(gameObject);
+        }
+    }
+}
