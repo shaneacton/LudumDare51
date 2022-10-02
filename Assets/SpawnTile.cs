4917ee58611d0b81515f4c61ad6597451211031a
Sasha Abramowitz <reallysasha@gmail.com>
2022-10-02 21:06:19 +0200

    lightning effect

--- Assets/SpawnTile.cs
+++ Assets/SpawnTile.cs
@@ -1,12 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.VFX;
 
 public class SpawnTile : MonoBehaviour
 {
 
-    public ParticleSystem EnemySpawnVFX;
-
+    public VisualEffect EnemySpawnVFX;
     private void Start() { EnemySpawnVFX.Stop(); }
 
 }
